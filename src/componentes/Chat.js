c502c9c1f33ff31ddc7c9200a3a673dfadffcccf
fedeziraldo <federrico04@gmail.com>
2020-09-07 22:00:34 -0300

    chat y actualiza paquetes

--- src/componentes/Chat.js
+++ src/componentes/Chat.js
@@ -1,21 +1,22 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import socketIOClient from "socket.io-client";
 const ENDPOINT = "http://localhost:4001";
+const socket = socketIOClient(ENDPOINT);
 
 function Chat({userId}) {
     const [mensajes, setMensajes] = useState([]);
     
     useEffect(() => {
-        entrar()
+        //entrar()
+        socket.off("chat");
+        socket.on("chat", msj => {
+            console.log(msj)
+            setMensajes([...mensajes, msj]);
+        });
         // // CLEAN UP THE EFFECT
         // return () => socket.disconnect();
         // //
-      },[]);
-    const socket = socketIOClient(ENDPOINT);
-    socket.on("chat", msj => {
-        console.log(msj)
-        setMensajes([...mensajes, msj]);
-    });
+      },[mensajes]);
     const entrar=()=>{
         socket.on('connect',()=>{
             socket.emit('entrarJuego',userId,function(resp){
