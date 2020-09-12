92b767b0bc8a22dd798f6e3253dee77e46aaf385
fedeziraldo <federrico04@gmail.com>
2020-09-12 00:13:35 -0300

    angola

--- src/componentes/Mapa.js
+++ src/componentes/Mapa.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Konva from 'konva';
 import { Stage, Layer, Star, Text, Image } from 'react-konva';
 import useImage from 'use-image';
@@ -6,11 +6,28 @@ import useImage from 'use-image';
 function Mapa() {
 
   const [image] = useImage('mapa.jpg');
-  console.log(image)
+  const [image2] = useImage('Paises/ANGOLA.png');
+  let angola
+
+  const rojificar = e => {
+    e.target['red'](Math.random()*255)
+    e.target.cache()
+    e.target.drawHitFromCache();
+    e.target.getLayer().batchDraw();
+    console.log("roj")
+  }
+
   return (
     <Stage width={window.innerWidth} height={window.innerHeight}>
       <Layer>
-        <Image image={image} />
+        <Image 
+          image={image2} 
+          onMouseOver={e => rojificar(e)}
+          filters={[Konva.Filters.RGB]}
+          ref={node => {
+            angola = node;
+          }}
+          />
       </Layer>
     </Stage>
   );
